224170fe66267428a6eb1d79ab4d25f21f18ab02
lazikiri <lazikiri@users.noreply.github.com>
2025-09-13 02:31:55 -0400

    fish: Added conf.d

---
+++ .config/fish/conf.d/00_init.fish
@@ -0,0 +1,6 @@
+set -U fish_greeting ""
+fastfetch
+
+zoxide init fish | source
+
+export EDITOR=nvim
---
+++ .config/fish/conf.d/10-aliases.fish
@@ -0,0 +1,29 @@
+alias c='clear'
+
+alias cd='z'
+alias cdi='zi'
+
+alias ls='eza -al --color=always --group-directories-first --icons'
+alias lt='eza -aT --color=always --group-directories-first --icons'
+
+alias cat='bat --style=full'
+
+alias lsblk='lsblk | bat --style=plain -l conf'
+
+alias grubupdate='sudo grub-mkconfig -o /boot/grub/grub.cfg'
+
+alias nf='fastfetch'
+alias ff='fastfetch'
+
+alias fzf='fzf --style full --color 16 --layout=reverse --height=30 --preview="bat -p --color=always {}"'
+
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit -m"
+alias gp="git push"
+alias gpl="git pull"
+alias gst="git stash"
+alias gsp="git stash; git pull"
+alias gfo="git fetch origin"
+alias gcheck="git checkout"
+alias gcredential="git config credential.helper store"
