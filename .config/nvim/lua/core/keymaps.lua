4706ea6dcbd345f3f73eccdebbe474c2e57577c5
lazikiri <lazikiri@users.noreply.github.com>
2025-09-15 16:19:25 -0400

    Added neovim config with neo tree and tokyo night theme

---
+++ .config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,20 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' ' -- Set leader key to space
+
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+vim.keymap.set('n', '<leader>nt', ':Neotree toggle <CR>') -- Leader + NT to open neotree
+
+vim.keymap.set('n', '<C-s>', '<cmd> w <CR>') -- Ctrl + S to save
+
+vim.keymap.set('n', '<C-q>', '<cmd> q <CR>', opts) -- CTRL + Q to quit
+
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>') -- Move focus to the left window
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>') -- Move focus to the right window
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>') -- Move focus to the lower window
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>') -- Move focus to the upper window
+
+vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts) -- Open new tab
+vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) -- Close current tab
+vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts) -- Go to next tab
+vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts) -- Go to previous tab
---
+++ .config/nvim/lua/core/options.lua
@@ -0,0 +1,28 @@
+vim.o.number = true
+vim.o.relativenumber = true -- Relative line nunbers
+
+vim.o.clipboard = 'unnamedplus' -- Sync OS clipboard and Neovim clipboard
+
+vim.o.wrap = false
+vim.o.linebreak = false -- Shows text as one long line
+
+vim.o.mouse = 'a' -- Enable mouse mode
+
+vim.o.undofile = true -- Save undo history
+
+vim.o.autoindent = true -- Copy indent from line when starting a new one
+
+vim.o.ignorecase = true
+vim.o.smartcase = true -- Case insensitive clipboard
+
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = 4 -- Set tab length to 4 spaces
+
+vim.o.expandtab = true -- Convert tabs to spaces
+
+vim.o.signcolumn = 'yes' -- Enable sign column
+
+vim.o.scrolloff = 10 -- Shows 10 a minimun of 10 lines on the top or bottom while scrolling 
+
+vim.o.confirm = true -- Ask if you want to save unsaved buffers when quitting
