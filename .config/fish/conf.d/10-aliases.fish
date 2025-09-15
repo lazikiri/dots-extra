ce70dae4fbd16b00c1cb3580d9b07707ff54251d
lazikiri <lazikiri@users.noreply.github.com>
2025-09-15 16:53:21 -0400

    fish: Removed bat

--- .config/fish/conf.d/10-aliases.fish
+++ .config/fish/conf.d/10-aliases.fish
@@ -6,10 +6,6 @@ alias cdi='zi'
 alias ls='eza -al --color=always --group-directories-first --icons'
 alias lt='eza -aT --color=always --group-directories-first --icons'
 
-alias cat='bat --style=full'
-
-alias lsblk='lsblk | bat --style=plain -l conf'
-
 alias grubupdate='sudo grub-mkconfig -o /boot/grub/grub.cfg'
 
 alias soft-reboot='sudo systemctl soft-reboot'
