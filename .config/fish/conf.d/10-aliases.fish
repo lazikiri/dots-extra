ac7dd82a702cc777341f92a80c2cade40b082e42
lazikiri <lazikiri@users.noreply.github.com>
2025-10-01 14:17:26 -0400

    fish: Removed git aliases as I don't use them, renamed grub alias

--- .config/fish/conf.d/10-aliases.fish
+++ .config/fish/conf.d/10-aliases.fish
@@ -6,7 +6,7 @@ alias cdi='zi'
 alias ls='eza -al --color=always --group-directories-first --icons'
 alias lt='eza -aT --color=always --group-directories-first --icons'
 
-alias grubupdate='sudo grub-mkconfig -o /boot/grub/grub.cfg'
+alias grub-update='sudo grub-mkconfig -o /boot/grub/grub.cfg'
 
 alias soft-reboot='sudo systemctl soft-reboot'
 
@@ -14,14 +14,3 @@ alias nf='fastfetch'
 alias ff='fastfetch'
 
 alias fzf='fzf --style full --color 16 --layout=reverse --height=30 --preview="cat {}"'
-
-alias gs="git status"
-alias ga="git add"
-alias gc="git commit -m"
-alias gp="git push"
-alias gpl="git pull"
-alias gst="git stash"
-alias gsp="git stash; git pull"
-alias gfo="git fetch origin"
-alias gcheck="git checkout"
-alias gcredential="git config credential.helper store"
