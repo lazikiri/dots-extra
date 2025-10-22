ce49cea868d0c6aa66a179ceda7ddbb28190f624
lazikiri <lazikiri@users.noreply.github.com>
2025-10-22 13:26:11 -0400

    nvim: Went back to tokyonight theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-    "navarasu/onedark.nvim",
+    "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-        vim.cmd [[colorscheme onedark]]
+        vim.cmd [[colorscheme tokyonight]]
     end
 }
